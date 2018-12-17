fbfb1ca168d4ca76b78b321795d2f51c9dd82d58
Yannick Dalbin <yannick@idvive.com>
2018-12-17 08:34:55 +0100

    TP3.2: Initialisez redux dans votre app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
+import { Provider } from "react-redux"
 import { render } from "react-dom"
 import React from "react"
 
+import configureStore from "./store"
 import Application from "./app"
 
-render(<Application name="Yannick"/>, document.getElementById("app"))
+const store = configureStore()
+
+const App = (
+  <Provider store={store}>
+    <Application name="Yannick"/>
+  </Provider>
+)
+
+render(<App/>, document.getElementById("app"))
